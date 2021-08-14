c0fdd2deca5884e76b5be8b86075166a70824818
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-14 11:50:41 -0300

    SQL Query with INNER JOIN

---
+++ uri2611/src/main/java/com/devsuperior/uri2611/respositories/MovieRepository.java
@@ -0,0 +1,21 @@
+package com.devsuperior.uri2611.respositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.uri2611.entities.Movie;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	@Query(nativeQuery = true, value = "SELECT m.id, m.name "
+			+ "FROM movies m "
+			+ "JOIN genres g ON (m.id_genres = g.id) "
+			+ "WHERE UPPER(g.description) = UPPER(:genreName)")
+	List<MovieMinProjection> search1(String genreName);
+	
+}
