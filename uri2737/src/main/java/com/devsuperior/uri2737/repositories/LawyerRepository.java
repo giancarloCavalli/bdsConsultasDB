43a4a409e8ee5b57c43e5300ed9927eaaeab5692
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-17 07:08:55 -0300

    SQL Query using UNION ALL (invalid syntax in JPQL)

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,31 @@
 package com.devsuperior.uri2737.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 
+@Repository
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
 
+	//Union All is an invalid syntax in JPQL. U can walk around it with other methods.
+	@Query(nativeQuery = true, value = "SELECT l.name, l.customers_number AS customersNumber "
+			+ "FROM lawyers l "
+			+ "WHERE l.customers_number = (SELECT MAX(l2.customers_number) FROM lawyers l2) "
+			+ " "
+			+ "UNION ALL "
+			+ " "
+			+ "SELECT l.name, l.customers_number "
+			+ "FROM lawyers l "
+			+ "WHERE l.customers_number = (SELECT MIN(l2.customers_number) FROM lawyers l2) "
+			+ " "
+			+ "UNION ALL "
+			+ " "
+			+ "SELECT 'Average', CAST(AVG(l.customers_number) AS INTEGER) "
+			+ "FROM lawyers l ")
+	List<LawyerMinProjection> search1();
 }
